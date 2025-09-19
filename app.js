4c9dee0e1e063188f867d0a78d3a9cdfc97c94df
julianresendiz <julian.resendiz@yahoo.com.mx>
2025-09-18 20:14:19 -0600

    funcion-agregar-amigo-no-vacio

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+function agregarAmigo(){
+    let inputAmigos = document.getElementById("amigo");
+    let amigo = inputAmigos.value;
+    console.log(amigo);
+
+    if (amigo === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+    }
+    
+    amigos.push(amigo);
+    console.log(amigos);
+}
